55ded385d8c90f471bfcd000c80cd755845d4d0f
trungdq306 <trungdq306@gmail.com>
2016-07-12 20:54:56 +0700

    Bảng student

---
+++ src/main/java/uet/usercontroller/DTO/StudentDTO.java
@@ -0,0 +1,22 @@
+package uet.usercontroller.DTO;
+
+/**
+ * Created by Trung on 7/11/2016.
+ */
+public class StudentDTO {
+    private int id;
+    private int userId;
+    private String studentName;
+
+    public int getId() { return id; }
+
+    public void setId(int id) { this.id = id; }
+
+    public int getUserId() { return userId; }
+
+    public void setUserId(int userId) {this.userId = userId; }
+
+    public String getStudentName() { return studentName; }
+
+    public void setStudentName(String studentName) { this.studentName = studentName; }
+}
